4060a8668d0e9b07028ce8468ce209b1a6ac3efa
Alexander Suetin <alexandersuetin@gmail.com>
2026-04-05 02:45:31 +0800

    temp

--- src/Admin/Settings.php
+++ src/Admin/Settings.php
@@ -7,16 +7,19 @@ namespace WpIrbis\Admin;
 final class Settings
 {
     public const OPTION = 'wp_irbis_settings';
+    private const PAGE_SLUG = 'wp-irbis';
+    private const SECTION_ID = 'wp_irbis_settings_section';
 
     public function register(): void
     {
         add_action('admin_init', [$this, 'registerSettings']);
+        add_action('admin_menu', [$this, 'registerMenu']);
     }
 
     public function registerSettings(): void
     {
         register_setting(
-            'general',
+            self::PAGE_SLUG,
             self::OPTION,
             [
                 'type' => 'array',
@@ -26,16 +29,51 @@ final class Settings
         );
 
         add_settings_section(
-            'wp_irbis_settings_section',
+            self::SECTION_ID,
             __('Настройки подключения к ИРБИС', 'wp-irbis'),
-            '__return_empty_string',
-            'general'
+            [$this, 'renderSection'],
+            self::PAGE_SLUG
         );
 
         $this->addField('host', __('IP сервера ИРБИС', 'wp-irbis'));
         $this->addField('login', __('Логин', 'wp-irbis'));
         $this->addField('password', __('Пароль', 'wp-irbis'), 'password');
         $this->addField('database', __('База данных', 'wp-irbis'));
+    }
+
+    public function registerMenu(): void
+    {
+        add_options_page(
+            __('ИРБИС', 'wp-irbis'),
+            __('ИРБИС', 'wp-irbis'),
+            'manage_options',
+            self::PAGE_SLUG,
+            [$this, 'renderPage']
+        );
+    }
+
+    public function renderPage(): void
+    {
+        if (! current_user_can('manage_options')) {
+            return;
+        }
+        ?>
+        <div class="wrap">
+            <h1><?php esc_html_e('ИРБИС', 'wp-irbis'); ?></h1>
+            <form action="options.php" method="post">
+                <?php
+                settings_fields(self::PAGE_SLUG);
+                do_settings_sections(self::PAGE_SLUG);
+                submit_button(__('Сохранить изменения', 'wp-irbis'));
+                ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    public function renderSection(): void
+    {
+        echo '<p>' . esc_html__('Укажите параметры подключения к серверу ИРБИС.', 'wp-irbis') . '</p>';
     }
 
     public function sanitize($value): array
@@ -67,8 +105,8 @@ final class Settings
                 >
                 <?php
             },
-            'general',
-            'wp_irbis_settings_section'
+            self::PAGE_SLUG,
+            self::SECTION_ID
         );
     }
 }
